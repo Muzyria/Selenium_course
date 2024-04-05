5e90eb87ce93311d94fcbfccf4ad80adfbdd64c2
FILA <fila090580@gmail.com>
2024-04-05 23:13:44 +0300

    add

--- Lesson_9/browser_options.py
+++ Lesson_9/browser_options.py
@@ -5,4 +5,17 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 
-driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+
+chrome_options = webdriver.ChromeOptions()
+# chrome_options.add_argument("--headless")
+# chrome_options.add_argument("--incognito")
+# chrome_options.add_argument("--ignore-certificate-errors")
+# chrome_options.add_argument("--window-size=400,400")
+chrome_options.add_argument("--disable-cache")
+
+
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=chrome_options)
+
+driver.get("https://www.freeconferencecall.com/en")
+
+time.sleep(5)
