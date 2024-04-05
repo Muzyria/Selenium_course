13e51c6fbcdb0d652fb8a70dc49495bc97f43c2c
FILA <fila090580@gmail.com>
2024-04-05 23:21:15 +0300

    add

--- Lesson_9/browser_options.py
+++ Lesson_9/browser_options.py
@@ -7,15 +7,23 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 
 chrome_options = webdriver.ChromeOptions()
+chrome_options.page_load_strategy = "normal"
+# chrome_options.page_load_strategy = "eager"
 # chrome_options.add_argument("--headless")
 # chrome_options.add_argument("--incognito")
 # chrome_options.add_argument("--ignore-certificate-errors")
-# chrome_options.add_argument("--window-size=400,400")
-chrome_options.add_argument("--disable-cache")
-
+chrome_options.add_argument("--window-size=1920,1080")
+# chrome_options.add_argument("--disable-cache")
 
 driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=chrome_options)
 
-driver.get("https://www.freeconferencecall.com/en")
+start_time = time.time()
+
+driver.get("https://beta.syncwise360.com/login")
+
+end_time = time.time()
+result = end_time - start_time
+
+print(result)
 
 time.sleep(5)
