f0c5b0d3dc473ab7b8efdd5a43b049a43b573081
FILA <fila090580@gmail.com>
2024-04-05 20:58:41 +0300

    add Lesson 4

---
+++ Lesson_8/click_tutor.py
@@ -0,0 +1,25 @@
+# selenium 4
+import time
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+
+
+driver.get("https://www.freeconferencecall.com/en")
+
+login_button = driver.find_element("xpath", '//a[@id="login-desktop"]')
+login_button.click()
+time.sleep(3)
+
+email_field = driver.find_element("xpath", '//input[@id="login_email"]')
+email_field.send_keys("123@122.com")
+print(email_field.get_attribute('value'))
+print(email_field.get_attribute('maxlength'))
+time.sleep(2)
+email_field.clear()
+email_field.send_keys('qwerreerytyui')
+
+time.sleep(3)
