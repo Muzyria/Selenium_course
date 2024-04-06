7cd27ba414d9ad36fceada0a9455f2095980b905
FILA <fila090580@gmail.com>
2024-04-07 00:47:50 +0300

    add

--- Lesson_11/explicit.py
+++ Lesson_11/explicit.py
@@ -4,6 +4,7 @@ import time
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
+
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
